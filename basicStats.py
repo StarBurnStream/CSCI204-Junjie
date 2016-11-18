4d9c6e3269b081efbb1e6a7ef9b8f835fc3414df
StarBurnStream <1106640847@qq.com>
2016-11-17 22:26:59 -0500

    hi

--- basicStats.py
+++ basicStats.py
@@ -1,5 +1,86 @@
 from sLink import *
+import random
 
+'''
+def fixDown(wlist, hlist, i):
+    if i * 2 >= len(hlist):
+        return wlist,hlist
+
+    if i * 2 + 1 == len(hlist) or hlist[i*2] >= hlist[i*2+1]:
+        if hlist[i*2] > hlist[i]:
+            value = hlist[i]
+            string = wlist[i]
+            hlist[i] = hlist[i*2]
+            wlist[i] = wlist[i*2]
+            hlist[i*2] = value
+            wlist[i*2] = string
+            return fixDown(wlist,hlist, i*2)
+        else:
+            return wlist,hlist
+
+    elif hlist[i*2] < hlist[i*2+1]:
+        if hlist[i*2+1] > hlist[i]:
+            value = hlist[i]
+            hlist[i] = hlist[i*2+1]
+            hlist[i*2+1] = value
+            string = wlist[i]
+            wlist[i] = wlist[i*2+1]
+            wlist[i*2+1] = string
+            return fixDown(wlist,hlist, i*2+1)
+        else:
+            return wlist,hlist
+'''
+
+def parent(index):
+    if index <= 1:
+        return -1
+    else:
+        return index // 2
+
+def leftChild(index):
+    return 2 * index
+
+def rightChild(index):
+    return 2 * index + 1
+
+def fixDown(wlist, hlist, i):
+    if leftChild(i) >= len(wlist):        
+        return wlist,hlist
+
+    if leftChild(i) < len(wlist) and hlist[leftChild(i)] > hlist[i]:
+        value = hlist[i]
+        hlist[i] = hlist[leftChild(i)]
+        hlist[leftChild(i)] = value
+        string = wlist[i]
+        wlist[i] = wlist[leftChild(i)]
+        wlist[leftChild(i)] = string
+        #print(wlist,hlist)
+
+    if rightChild(i) >= len(wlist):
+        return wlist, hlist
+    
+    if rightChild(i) < len(wlist) and hlist[rightChild(i)] > hlist[i]:
+        value = hlist[i]
+        hlist[i] = hlist[rightChild(i)]
+        hlist[rightChild(i)] = value
+        string = wlist[i]
+        wlist[i] = wlist[rightChild(i)]
+        wlist[rightChild(i)] = string
+        #print(wlist,hlist)
+    return wlist,hlist
+
+def heapRemove(wlist, hlist):
+    if 1 == len(hlist) - 1:
+        hlist.pop()
+        wlist.pop()
+        return wlist,hlist
+    print(wlist,hlist)
+    hlist[1] = hlist.pop()
+    wlist[1] = wlist.pop()
+    for i in range(len(wlist)-1,0,-1):
+        print(wlist,hlist)
+        wlist, hlist = fixDown(wlist,hlist, i)
+    return wlist,hlist
 
 class BasicStats:
     """
@@ -122,6 +203,72 @@ class BasicStats:
                     bottomDict[keys] = freqDict[keys]
                     del bottomDict[maxKey]
         return bottomDict
+
+
+
+
+    def topNHeap(self, wordlist, n,):
+        hlist = [0]
+        wlist = [0]
+        topNHlist = []
+        topNWlist = []
+        for word in wordlist:
+            try:
+                index = wlist.index(word)
+                hlist[index] += 1
+            except ValueError:
+                wlist.append(word)
+                hlist.append(1)
+        for i in range(len(wlist)-1,0,-1):
+            wlist, hlist = fixDown(wlist,hlist, i)
+        for i in range(n):
+            topNHlist.append(hlist[1])
+            topNWlist.append(wlist[1])
+            wlist,hlist = heapRemove(wlist,hlist)
+        return topNWlist,topNHlist
+            
+
+
+    
+
+    def bottomNHeap(self,heapList):
+        pass
+
+
+
+
+def main():
+    wordList = []
+    words = ['happy','sad','angry','blue','anxious']
+    for i in range(10):
+        word = random.choice(words)
+        wordList.append(word)
+    print(wordList)
+    a = BasicStats()
+    aList,bList = a.topNHeap(wordList,3)
+    print(aList,bList)
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 """
 Better way to do both at the same time:
--- textFilter.py
+++ textFilter.py
@@ -5,7 +5,7 @@ class TextFilter:
     def __init__(self,aDocument,aList):
         self.filtList=aList
         self.document=aDocument
-        self.sentence=self.document.sentence
+        self.sentence=self.document._Document__sentence
 
     def normSpace(self):
         for s in range(len(self.sentence)):
@@ -28,7 +28,7 @@ class TextFilter:
             sen=self.sentence[s].sentence
             newSen=''
             for i in range(len(sen)):
-                if ord(sen[i]) in range(97,123) or ord(sen[i]) in range(65,91) or ord(sen[i]) in range(48,58):
+                if ord(sen[i]) in range(97,123) or ord(sen[i]) in range(65,91) or ord(sen[i]) in range(48,58) or ord(sen[i]) == 32:
                    newSen+=sen[i]
             self.sentence[s].sentence=newSen
 
@@ -37,12 +37,25 @@ class TextFilter:
             sen=self.sentence[s].sentence
             newSen=''
             for i in sen:
-                print(i,ord(i))
+                #print(i,ord(i))
                 if ord(i) not in range(48,58):
                     newSen+=i
             self.sentence[s].sentence=newSen
 
+    def stripFile(self):
+        '''
+        Should be used after deleteNull and no need to strip space any more!
+        '''
+        file = open('filterwords.txt')
+        text = file.read()
+        wordList = text.split()
+        for i in range(len(self.sentence)):
+            sen = self.sentence[i].sentence
+            words = sen.split()
+            sen = ' '.join([ x for x in words if x not in wordList])
+                
 
+    
     def apply(self):
         for s in self.filtList:
             if s=='normSpace':
@@ -63,10 +76,11 @@ def main():
     filename='1.txt'
     a=document.Document(filename)
     a.generateWhole()
-    b=TextFilter(a,['normSpace','normCase','deleteNull','stripNum'])
+    #b=TextFilter(a,['normSpace','normCase','deleteNull','stripNum'])
+    b=TextFilter(a,['stripFile'])
     b.apply()
     
-    for i in a.sentence:
+    for i in a._Document__sentence:
         
         print(i.sentence)
 
